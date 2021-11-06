d86ba4901c4daf7a2fbde8508573e0f9c7e58bcd
hueseyinsoezer <huseyinsozer97@hotmail.com>
2021-11-06 14:14:12 +0300

    +Added colliders +Added movement on player

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] private float mSpeed;
+    private float mSpeedX, mSpeedY;
+    private Rigidbody2D pBody;
+
+
+    void Start()
+    {
+        pBody = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        mSpeedX = Input.GetAxisRaw("Horizontal");
+        mSpeedY = Input.GetAxisRaw("Vertical");
+        pBody.velocity = new Vector2(mSpeedX* mSpeed, pBody.velocity.y);
+        pBody.velocity = new Vector2(pBody.velocity.x,mSpeedY*mSpeed);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            Destroy(collision.gameObject);
+            Debug.Log("Duvar ile temas edildi!");
+        }
+        
+    }
+}
